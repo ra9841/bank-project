92621f261fd8163a56775ac9e9d0d0166bdf5d66
Rabin <rabin.shrestha7@gmail.com>
2024-10-19 14:06:56 -0700

    practising get and delete mapping

---
+++ src/main/java/com/rabin/banking_system_project/configuration/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.rabin.banking_system_project.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
+                    authorizationManagerRequestMatcherRegistry
+                            .requestMatchers("/customers","/customers/**","/actuator/metrics/**").permitAll()
+                            .requestMatchers("/security/**").authenticated();
+                })
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> {
+                    httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/rabin/banking_system_project/controller/CustomerController.java
+++ src/main/java/com/rabin/banking_system_project/controller/CustomerController.java
@@ -4,10 +4,9 @@ import com.rabin.banking_system_project.dto.CustomerDto;
 import com.rabin.banking_system_project.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
@@ -16,8 +15,19 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @PostMapping
-    private ResponseEntity<CustomerDto> registrationOfCustomerRecord(@RequestBody CustomerDto customerDto){
+    @PostMapping //localhost:8080/customers
+    public ResponseEntity<CustomerDto> registrationOfCustomerRecord(@RequestBody CustomerDto customerDto){
      return ResponseEntity.ok(customerService.savingTheCustomerRecord(customerDto));
     }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDto>> gettingListOfRecord() {
+        return ResponseEntity.ok(customerService.listOfCustomerRecord());
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<String> deletingTheRecord(@PathVariable String email){
+      return ResponseEntity.ok(customerService.deletingTheRecordByEmail(email));
+    }
+
 }
